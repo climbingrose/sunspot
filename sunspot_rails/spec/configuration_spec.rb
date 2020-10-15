8a7ad86a27a728f5df9628b3c048e64f2572e7ef
kukula <anatolkukula@gmail.com>
2020-10-15 14:00:13 +1100

    Remove value that triggers sec scanner (#12)
    * Remove value that triggers sec scanner
    
    * Update README.md

--- sunspot_rails/spec/configuration_spec.rb
+++ sunspot_rails/spec/configuration_spec.rb
@@ -259,7 +259,7 @@ end
 
 describe Sunspot::Rails::Configuration, "with ENV['WEBSOLR_URL'] including userinfo" do
   before(:all) do
-    ENV['WEBSOLR_URL'] = 'https://user:pass@index.websolr.test/solr/a1b2c3d4e5f'
+    ENV['WEBSOLR_URL'] = ''
   end
 
   before(:each) do
@@ -272,7 +272,7 @@ describe Sunspot::Rails::Configuration, "with ENV['WEBSOLR_URL'] including useri
   end
 
   it "should include username and passowrd" do
-    @config.userinfo.should == "user:pass"
+    @config.userinfo.should == ""
   end
 
   it "should set the scheme to https" do
